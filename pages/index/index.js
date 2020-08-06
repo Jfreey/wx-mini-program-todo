0f441fb3989a03367abe7c74bbb507919cbf9524
Jfreey <jfreey@qq.com>
2020-08-06 15:37:40 +0800

    Add filter function

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,18 @@ Page({
         userInfo: null,
         todoContent: '',
         todoList: [],
+        filterList: [
+            { name: '全部', value: 'all', checked: true },
+            { name: '未完成', value: 'active', checked: false },
+            { name: '已完成', value: 'complete', checked: false },
+        ],
+        selectAll: false,
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
     },
-    onLoad(options) {},
+    onLoad(options) {
+        this.setData({ todoList: wx.getStorageSync('todoList') || [] });
+    },
     onReady() {},
     onShow() {},
     onHide() {},
@@ -35,15 +43,71 @@ Page({
             },
             () => {
                 this.setData({
+                    selectAll: false,
                     todoContent: '',
                 });
+                wx.setStorageSync('todoList', [...this.data.todoList]);
             },
         );
     },
     selectHandle(e) {
         const { index } = e.currentTarget.dataset;
+        const checked = !this.data.todoList[index].checked;
+        this.setData(
+            {
+                [`todoList[${index}].checked`]: checked,
+            },
+            () => {
+                this.setData({
+                    selectAll: this.data.todoList.every((item) => item.checked),
+                });
+                const todoListStorage = wx.getStorageSync('todoList');
+                todoListStorage[index].checked = checked;
+                wx.setStorageSync('todoList', todoListStorage);
+            },
+        );
+    },
+    selectAll() {
+        const selectAll = !this.data.selectAll;
+        this.setData(
+            {
+                selectAll,
+                todoList: this.data.todoList.map((item) => {
+                    item.checked = selectAll;
+                    return item;
+                }),
+            },
+            () => {
+                wx.setStorageSync(
+                    'todoList',
+                    this.data.todoList.map((item) => {
+                        item.checked = selectAll;
+                        return item;
+                    }),
+                );
+            },
+        );
+    },
+    filter(e) {
+        const { value } = e.currentTarget.dataset;
+        let todoList;
+        switch (value) {
+            case 'all':
+                todoList = [...wx.getStorageSync('todoList')];
+                break;
+            case 'active':
+                todoList = wx
+                    .getStorageSync('todoList')
+                    .filter((item) => !item.checked);
+                break;
+            case 'complete':
+                todoList = wx
+                    .getStorageSync('todoList')
+                    .filter((item) => item.checked);
+                break;
+        }
         this.setData({
-            [`todoList[${index}].checked`]: !this.data.todoList[index].checked,
+            todoList,
         });
     },
 });
